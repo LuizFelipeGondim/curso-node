bb0c87fba8e20da538ae9bd75069de5dde2547d3
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2024-01-06 01:36:45 -0300

    database version

--- fundamentos/src/server.js
+++ fundamentos/src/server.js
@@ -1,8 +1,11 @@
 import http from 'node:http' //Usa-se node: por http ser uma API interna do node.
-import { json } from './middlewares/json.js';
-														 //Nao esstou usando require porque mudei o metodo de importacao no package.json
+														//Nao esstou usando require porque mudei o metodo de importacao no package.json
 
-const users = [];
+import { json } from './middlewares/json.js';
+import { Database } from './middlewares/database.js';
+import { randomUUID } from 'node:crypto';
+
+const database = new Database();
 
 const server = http.createServer(async (req, res) => {
 	const { method, url } = req;
@@ -10,16 +13,21 @@ const server = http.createServer(async (req, res) => {
 	await json(req, res);
 
 	if (method === 'GET' && url === '/users') {
+		const users = database.select('users');
+
 		return res.end(JSON.stringify(users));
 	}
 
 	if (method === 'POST' && url === '/users') {
 		const { name, email } = req.body;
 
-		users.push({
+		const user = {
+			id: randomUUID(),
 			name,
 			email
-		});
+		};
+
+		database.insert('users', user);
 
 		return res.writeHead(201).end();
 	}
