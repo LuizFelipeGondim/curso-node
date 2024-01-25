5e363e99eaa2da02f9c1c9c8146e68848a8f98c5
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2024-01-25 20:48:41 -0300

    database config

--- rotas-http/src/server.ts
+++ rotas-http/src/server.ts
@@ -1,15 +1,18 @@
 import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
-// http://localhost:3333/hello
-app.get('/helllo', () => {
-  return 'Hello world'
+app.get('/hello', () => {
+  const tables = knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running!')
